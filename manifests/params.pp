0701a1c2cbfa75d8e608b184a08da6ea79496c67
Robert Stuart <stuartrobert@users.noreply.github.com>
2021-03-26 09:51:26 +1000

    Update params.pp
    remove package_source_location

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,17 +1,17 @@
 # params for ncpa
 class ncpa::params {
-  $allowed_hosts              = []
-  $service_state              = 'running'
-  $service_enable             = true
-  $package_source_location    = 'https://assets.nagios.com/downloads/ncpa'
-  $package_name               = 'NCPA'
-  $download_destination       = 'c:/temp'
-  $config_template            = 'ncpa/ncpa.cfg.epp'
-  $install_path               = 'C:\Program Files (x86)\Nagios\NCPA'
-  $proxy_url                  = undef
-  $password                   = undef
-  $chocolatey_provider        = false
-  $chocolatey_package_name    = 'ncpa'
+  $allowed_hosts           = []
+  $service_state           = 'running'
+  $service_enable          = true
+  $package_source          = 'https://assets.nagios.com/downloads/ncpa'
+  $package_name            = 'NCPA'
+  $download_destination    = 'c:/temp'
+  $config_template         = 'ncpa/ncpa.cfg.epp'
+  $install_path            = 'C:\Program Files (x86)\Nagios\NCPA'
+  $proxy_url               = undef
+  $password                = undef
+  $chocolatey_provider     = false
+  $chocolatey_package_name = 'ncpa'
 
   if $chocolatey_provider {
     $package_version = 'latest'
