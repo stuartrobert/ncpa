efc003a5211eb70790897e3753437ebeff357c23
Robert Stuart <robertmstuart@gmail.com>
2021-03-25 16:50:41 +1000

    initial add of ncpa.cfg.epp template and params

---
+++ manifests/params.pp
@@ -0,0 +1,22 @@
+# params for ncpa
+class ncpa::params {
+  $allowed_hosts              = []
+  $service_state              = 'running'
+  $service_enable             = true
+  $package_source_location    = 'https://assets.nagios.com/downloads/ncpa/'
+  $package_name               = 'NCPA x86'
+  $package_source             = ''
+  $download_destination       = 'c:/temp'
+  $config_template            = 'ncpa/ncpa.cfg.epp'
+  $install_path               = 'C:\Program Files (x86)\Nagios\NCPA'
+  $proxy_url                  = undef
+  $password                   = undef
+  $chocolatey_provider        = false
+  $chocolatey_package_name    = 'ncpa'
+
+  if $chocolatey_provider {
+    $package_version = '2.3.1'
+  } else {
+    $package_version = 'latest'
+  }
+}
