ded04ab42551a9ccc58239b593a79abb54ad3772
Robert Stuart <robertmstuart@gmail.com>
2021-03-25 16:19:20 +1000

    Initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,18 @@
+# module that installs ncpa
+class ncpa (
+  $allowed_hosts                       = $ncpa::params::allowed_hosts,
+  $service_state                       = $ncpa::params::service_state,
+  $service_enable                      = $ncpa::params::service_enable,
+  String $package_source_location      = $ncpa::params::package_source_location,
+  String $package_source               = $ncpa::params::package_source,
+  String $package_name                 = $ncpa::params::package_name,
+  $download_destination                = $ncpa::params::download_destination,
+  Optional[String[1]] $package_version = $ncpa::params::package_version,
+  Optional[String[1]] $proxy_url       = $ncpa::params::proxy_url,
+) inherits ncpa::params {
+
+  class { 'ncpa::install': }
+  class { 'ncpa::config': }
+  class { 'ncpa::service': }
+
+}
