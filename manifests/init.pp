1b20055769b6a25eecec32dd12116d3002cfcdca
Robert Stuart <robertmstuart@gmail.com>
2021-03-27 15:06:23 +1000

    added config_template and config_content params

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,13 @@
 # module that installs ncpa
 class ncpa (
-  $allowed_hosts                       = $ncpa::params::allowed_hosts,
-  $service_state                       = $ncpa::params::service_state,
+  Array $allowed_hosts                 = $ncpa::params::allowed_hosts,
+  String $service_state                = $ncpa::params::service_state,
   Boolean $service_enable              = $ncpa::params::service_enable,
   String $package_source               = $ncpa::params::package_source,
   String $package_name                 = $ncpa::params::package_name,
-  $download_destination                = $ncpa::params::download_destination,
+  String $download_destination         = $ncpa::params::download_destination,
+  String $config_template              = $ncpa::params::config_template,
+  Optional[String] $config_content     = undef,
   Optional[String[1]] $package_version = $ncpa::params::package_version,
   Optional[String[1]] $proxy_url       = $ncpa::params::proxy_url,
 ) inherits ncpa::params {
